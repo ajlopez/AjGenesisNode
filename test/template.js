df607105b1b9b8be454284bd0bb3e7a1dfe1aa7d
ajlopez <webmaster@ajlopez.com>
2012-05-22 10:51:24 -0300

    template expansion with first tests

--- test/template.js
+++ test/template.js
@@ -2,15 +2,27 @@
 var $ = require('../lib/template.js')
   , assert = require('assert');
   
-var tmp = $.compileTemplate("Hello ${x}");
-
-var output = {
-	content: '',
-	write: function(text) { this.content += text; }
+function OutputWriter() {
+    this.content = '';
 };
 
-var = "World";
+OutputWriter.prototype.write = function(text)
+{
+    this.content += text;
+};
 
-tmp(output);
+assert.equal(doTemplate("Hello, World"), "Hello, World");
 
-assert.equal(output.content, "Hello World");
+global.x = "World";
+
+assert.equal(doTemplate("Hello, ${x}"), "Hello, World");
+assert.equal(doTemplate("1 + 2 = ${1+2}"), "1 + 2 = 3");
+
+function doTemplate($text)
+{
+    var $output = new OutputWriter();
+    var $template = $.compileTemplate($text);
+    $template($output);
+    return $output.content;
+}
+
