6672e332223cebf4a2a84cbbd95da00c91eeb5ca
ajlopez <webmaster@ajlopez.com>
2013-06-01 17:59:59 -0300

    process template with target

--- test/process.js
+++ test/process.js
@@ -1,6 +1,7 @@
 
 var ajgenesis = require('..'),
     path = require('path'),
+    fs = require('fs'),
     assert = require('assert');
 
 // Process simple model
@@ -25,3 +26,25 @@ assert.ok(model.author);
 assert.ok(model.company);
 assert.ok(model.year);
 
+// Process simple model with template and file
+    
+var model = { };
+
+if (!fs.existsSync('build'))
+    fs.mkdirSync('build');
+
+var targetname = path.join('build', 'simpletarget.txt');
+
+if (fs.existsSync(targetname))
+    fs.unlinkSync(targetname);
+
+ajgenesis.process(model, [
+    path.join('test', 'files', 'simplemodel.json'), 
+    path.join('test', 'files', 'simpletemplate.tpl'),
+    targetname]);   
+
+assert.ok(model);
+assert.ok(model.title);
+assert.ok(model.author);
+
+assert.ok(fs.existsSync(targetname));
