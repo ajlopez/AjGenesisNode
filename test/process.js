ed78339280762bebb7e71cd343458f186bc2dbfa
ajlopez <webmaster@ajlopez.com>
2013-06-01 17:45:45 -0300

    Additional test with two models

--- test/process.js
+++ test/process.js
@@ -2,6 +2,8 @@
 var ajgenesis = require('..'),
     path = require('path'),
     assert = require('assert');
+
+// Process simple model
     
 var model = { };
 
@@ -10,4 +12,16 @@ ajgenesis.process(model, [path.join('test', 'files', 'simplemodel.json')]);
 assert.ok(model);
 assert.ok(model.title);
 assert.ok(model.author);
+
+// Process two simple models
+    
+var model = { };
+
+ajgenesis.process(model, [path.join('test', 'files', 'simplemodel.json'), path.join('test', 'files', 'simpleproject.json')]);
+
+assert.ok(model);
+assert.ok(model.title);
+assert.ok(model.author);
+assert.ok(model.company);
+assert.ok(model.year);
 
