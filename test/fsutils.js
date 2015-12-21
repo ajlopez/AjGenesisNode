de3285bbd08b39b1e56e5a229e797627c37669e8
ajlopez <webmaster@ajlopez.com>
2015-12-21 09:13:44 -0300

    fsutils copy file

--- test/fsutils.js
+++ test/fsutils.js
@@ -1,4 +1,5 @@
 
+var fs = require('fs');
 var fsutils = require('../lib/fsutils');
 var path = require('path');
 
@@ -11,3 +12,21 @@ exports['is directory'] = function (test) {
     test.strictEqual(fsutils.isDirectory('test'), true);
     test.strictEqual(fsutils.isDirectory(path.join('test', 'fsutils.js')), false);
 }
+
+exports['copy file'] = function (test) {
+    test.async();
+    
+    var source = path.join('test', 'files', 'simplemodel.json');
+    var target = path.join('test', 'files2', 'simplemodel.txt');
+    
+    fsutils.createDirectory('test', 'files2');
+    
+    fsutils.copyFile(source, target, function (err) {
+        test.equal(err, null);
+        test.ok(fs.existsSync(target));
+        fs.unlinkSync(target);
+        fs.rmdirSync(path.join('test', 'files2'));
+        test.done();
+    });
+}
+
