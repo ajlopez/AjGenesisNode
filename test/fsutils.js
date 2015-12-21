1c0b7b55f46dc3a4408ca5cbf7aae6b16f374c11
ajlopez <webmaster@ajlopez.com>
2015-12-21 09:16:10 -0300

    create copy directory

--- test/fsutils.js
+++ test/fsutils.js
@@ -30,3 +30,49 @@ exports['copy file'] = function (test) {
     });
 }
 
+exports['copy directory'] = function (test) {
+    test.async();
+    
+    var source = path.join('test', 'ajgenesis');
+    var target = path.join('test', 'ajgenesis2');
+    
+    fsutils.copyDirectory(source, target, function (err) {
+        test.equal(err, null);
+        test.ok(fs.existsSync(target));
+        test.ok(fs.existsSync(path.join(target, 'tasks')));
+        test.ok(fs.existsSync(path.join(target, 'tasks', 'simple.js')));
+        test.ok(fs.existsSync(path.join(target, 'modules')));
+        test.ok(fs.existsSync(path.join(target, 'modules', 'module1')));
+        test.ok(fs.existsSync(path.join(target, 'modules', 'module1', 'simple.js')));
+        removeDirSync(target);
+        test.done();
+    });
+}
+
+exports['create directory'] = function (test) {
+    fsutils.createDirectory('test', 'ajgenesis3', 'models');
+    var target = path.join('test', 'ajgenesis3', 'models');
+    
+    test.ok(fs.existsSync(target));
+    removeDirSync(target);
+    removeDirSync(path.join('test', 'ajgenesis3'));
+}
+
+function removeDirSync(dirname) {
+    var filenames = fs.readdirSync(dirname);
+    
+    filenames.forEach(function (filename) {
+        filename = path.join(dirname, filename);
+        
+        if (fsutils.isDirectory(filename))
+            removeDirSync(filename);
+        else
+            removeFileSync(filename);
+    });
+    
+    fs.rmdirSync(dirname);
+}
+
+function removeFileSync(filename) {
+    fs.unlinkSync(filename);
+}
